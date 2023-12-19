9b64dd32c677c1f3c91afa52fe420443f0b2a734
ashish082003 <aj9172243856@gmail.com>
2023-12-19 10:26:49 +0530

    final

--- app.js
+++ app.js
@@ -17,8 +17,17 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true},{useUnifiedTopology: true });
-
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+}).then(()=>{
+   console.log(`connection to database established`)
+}).catch(err=>{
+   console.log(`db error ${err.message}`);
+   process.exit(-1)
+})
 const postSchema = {
   title: String,
   content: String
